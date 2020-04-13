e2466447263deecd3d809169e62e19ef5ca81a01
Manuel Soto <repo.micorreo@spamgourmet.com>
2020-04-13 20:44:34 -0300

    add handlers alias

--- src/Action.jsx
+++ src/Action.jsx
@@ -1,20 +1,22 @@
 import React, { useMemo } from 'react';
-import {Context as CounterContext} from './CountContext'
+import { Context as CounterContext } from './CountContext'
 import { useContext } from 'react'
 
-const Action  = ()=> {
+const Action = () => {
     const ctx = useContext(CounterContext)
-    const add10 = useMemo(() => ()=>ctx.actions.incrementBy(10), [ctx.actions.incrementBy])
-    const sub10 = useMemo(() => ()=>ctx.actions.decrementBy(10), [ctx.actions.decrementBy])
+    const sub1 = ctx.actions.decrement
+    const add1 = ctx.actions.increment
+    const sub10 = useMemo(() => () => ctx.actions.decrementBy(10), [ctx.actions.decrementBy])
+    const add10 = useMemo(() => () => ctx.actions.incrementBy(10), [ctx.actions.incrementBy])
 
     return (
-    <section>
-        <button onClick={sub10}>-10</button>
-        <button onClick={ctx.actions.decrement}>-</button>
-        <span>{ctx.counter} </span>
-        <button onClick={ctx.actions.increment}>+</button>
-        <button onClick={add10}>+10</button>
-    </section>
+        <section>
+            <button onClick={sub10}>-10</button>
+            <button onClick={sub1}>-</button>
+            <span>{ctx.counter} </span>
+            <button onClick={add1}>+</button>
+            <button onClick={add10}>+10</button>
+        </section>
     )
 }
 
